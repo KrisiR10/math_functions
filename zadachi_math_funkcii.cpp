56fca005fd93ccea358e33a87429b2c2bde166d7
KrisiR10 <pishtovka123@gmail.com>
2025-03-19 15:47:24 +0200

    Add files via upload

---
+++ zadachi_math_funkcii.cpp
@@ -0,0 +1,135 @@
+
+
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    // ------------------------------------------- zadacha 1
+
+    /*int a;
+    int x;
+    int z;
+
+    printf("Value a= ");
+    scanf_s("%d", &a);
+
+    printf("Value x= ");
+    scanf_s("%d", &x);
+
+    printf("Value z= ");
+    scanf_s("%d", &z);
+
+    double y = 4 * pow(a, 2) + 34 * x + pow(z, 5);
+    printf("y: %.2lf", y);*/
+
+    // ------------------------------------------- zadacha 2
+
+    /*int x;
+    int a;
+
+    printf("Value x= ");
+    scanf_s("%d", &x);
+
+    printf("Value a= ");
+    scanf_s("%d", &a);
+
+    double y = ((2.0 / 3) * pow(x, 2)) - ((1.0 / 7) * x) + ((1.0 / 5) * a);
+    printf("y :%.2lf", y);*/
+    
+    // ------------------------------------------- zadacha 3
+
+    /*int x;
+    int y;
+
+    printf("Value x= ");
+    scanf_s("%d", &x);
+
+    printf("Value y= ");
+    scanf_s("%d", &y);
+
+    double z = 4 / (5 * x * y + 12);
+    printf("%.2lf", z);*/
+
+    // ------------------------------------------- zadacha 4
+
+    /*int x;
+
+    printf("Value x= ");
+    scanf_s("%d", &x);
+
+    double y = pow(x, 2) - (1.0 / x);
+    printf("%.2lf", y);*/
+
+    // ------------------------------------------- zadacha 5
+
+    /*int x;
+    int y;
+
+    printf("Value x= ");
+    scanf_s("%d", &x);
+
+    printf("Value y= ");
+    scanf_s("%d", &y);
+
+    double z = ((2 * x - 1) / 5) + ((x + 1) / (2 * pow(y, 2) + y));
+    printf("%.2lf", z);*/
+
+    // ------------------------------------------- zadacha 6
+
+    /*int x;
+
+    printf("Value x= ");
+    scanf_s("%d", &x);
+
+    double y = (1.0 / x) + pow((sqrt(3 - x) - sqrt(2 + x)),2);
+    printf("%.2lf", y);*/
+
+    // ------------------------------------------- zadacha 7
+
+    /*int x;
+    int y;
+
+    printf("Value x= ");
+    scanf_s("%d", &x);
+
+    printf("Value y= ");
+    scanf_s("%d", &y);
+
+    double z = pow((x + y), 3);
+    printf("%.2lf", z);*/
+
+    // ------------------------------------------- zadacha 8
+
+    /*int x;
+
+    printf("Value x= ");
+    scanf_s("%d", &x);
+
+    double y = pow(x, 2) + (9 * x) - pow(sqrt(x), 3);
+    printf("%.2lf", y);*/
+
+    // ------------------------------------------- zadacha 9
+   
+    /*double y = pow((1 - sqrt(2)) / sqrt(3),2) - pow((sqrt(3) + 2) / sqrt(2),2);
+    printf("%.2lf", y);*/
+
+    // ------------------------------------------- zadacha 10   
+    int x;
+    int y;
+
+    printf("Value x= ");
+    scanf_s("%d", &x);
+
+    printf("Value y= ");
+    scanf_s("%d", &y);
+
+    double z = pow(x, 5) - (2 * x * y) + pow(25, 2);
+    printf("%.2lf", z);
+
+
+}
+
+
+
